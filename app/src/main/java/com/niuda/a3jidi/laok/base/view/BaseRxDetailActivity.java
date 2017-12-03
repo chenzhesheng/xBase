ad9898b1de4a4523d088257b4e492b36da057a74
chenzhesheng <czsprivatemail@sina.com>
2017-12-04 00:24:33 +0800

    基本框架

---
+++ app/src/main/java/com/niuda/a3jidi/laok/base/view/BaseRxDetailActivity.java
@@ -0,0 +1,26 @@
+package com.niuda.a3jidi.laok.base.view;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+/**
+ * (￣▽￣)"
+ *
+ * @version V1.0 :统一管理所有的订阅生命周期
+ * @author: Administrator
+ * @date: 2017-11-11 14:07
+ */
+public abstract class BaseRxDetailActivity extends BaseActivity {
+
+    private CompositeDisposable compositeDisposable;
+
+    public void addDisposable(Disposable disposable) {
+        if (compositeDisposable == null) {
+            compositeDisposable = new CompositeDisposable();
+        }
+        compositeDisposable.add(disposable);
+    }
+
+    public void dispose() {
+        if (compositeDisposable != null) compositeDisposable.dispose();
+    }
+}
